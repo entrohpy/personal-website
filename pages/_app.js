1640f2b32b9094347ba48e2f95359ceaac629d02
Rohit Nema <entrohpy@icloud.com>
2022-03-24 08:13:55 +0530

    Add fontawesome icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import '../styles/globals.css';
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 const App = ({ Component, pageProps }) => {
   return (
