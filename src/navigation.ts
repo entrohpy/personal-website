3144edf71a8e47453556653ad3dce6a4940789c2
Ro <ro@nema.page>
2026-04-14 17:14:46 -0700

    Initiate astro website

---
+++ src/navigation.ts
@@ -0,0 +1,42 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'About',
+      href: getPermalink('/#about'),
+    },
+    {
+      text: 'Papers',
+      href: getPermalink('/#papers'),
+    },
+    {
+      text: 'Talks',
+      href: getPermalink('/#talks'),
+    },
+    {
+      text: 'Teaching',
+      href: getPermalink('/#teaching'),
+    },
+    {
+      text: 'Misc.',
+      href: getPermalink('/misc'),
+    },
+  ],
+  actions: [{ text: 'Email me', href: 'mailto:rnema@cs.stanford.edu', target: '_blank' }],
+};
+
+export const footerData = {
+  secondaryLinks: [
+    { text: 'About', href: getPermalink('/terms') },
+    { text: 'Papers', href: getPermalink('/privacy') },
+    { text: 'Talks', href: getPermalink('/privacy') },
+    { text: 'Contact', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/entrohpy' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/entrohpy">Rohit Nema</a> using <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo/astrowind">AstroWind</a>· All rights reserved.
+  `,
+};
